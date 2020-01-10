9179d730e4d4a427115ab4ff1e688c762682a994
mcjsitoy <33475916+mcjsitoy@users.noreply.github.com>
2020-01-10 08:53:37 +0800

    1/10/2020

---
+++ app/src/main/java/com/example/grabguyod/after.java
@@ -0,0 +1,14 @@
+package com.example.grabguyod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class after extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after);
+    }
+}
